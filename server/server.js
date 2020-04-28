281e8d51d5fa2f7d7fb2ec97ea84549dd470809d
Myron <dubtwee@gmail.com>
2020-04-28 11:25:57 -0500

    creating API endpoint to get all genres

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 const moviesRouter = require('./routers/movies.router');
+const genresRouter = require('./routers/genres.router');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
@@ -11,6 +12,7 @@ app.use(express.static('build'));
 /** ---------- ROUTES ---------- **/
 // register movies router
 app.use('/api/movies', moviesRouter);
+app.use('/api/genres', genresRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
