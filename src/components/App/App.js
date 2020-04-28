4c45cd68e76ec4278d6c41ea158dd33cd9e6385b
Myron <dubtwee@gmail.com>
2020-04-27 23:04:43 -0500

    creating basic pages for the Home and Edit views

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 // PAGE COMPONENTS
 import DetailsPage from '../pages/DetailsPage/DetailsPage';
+import HomePage from '../pages/HomePage/HomePage';
+import EditPage from '../pages/EditPage/EditPage';
 
 class App extends Component {
   // Renders the entire app on the DOM
@@ -11,9 +13,9 @@ class App extends Component {
     return (
       <div className="App">
         <Router>
-          {/* <Route exact path="/" component={DetailsPage} /> */}
+          <Route exact path="/" component={HomePage} />
           <Route path="/details" component={DetailsPage} />
-          {/* <Route path="/edit" component={DetailsPage} /> */}
+          <Route path="/edit" component={EditPage} />
         </Router>
       </div>
     );
---
+++ src/components/pages/EditPage/EditPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Edit</h2>
+      </div>
+    );
+  }
+}
+
+export default EditPage;
