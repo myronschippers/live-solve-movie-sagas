e53b0f1c17a352db282da5ef65d2daa9f54be85b
Myron <dubtwee@gmail.com>
2020-04-27 21:28:25 -0500

    creatign junction table for movies and genres relationship

--- database.sql
+++ database.sql
@@ -11,12 +11,15 @@ CREATE TABLE "genres" (
   "name" VARCHAR(80) NOT NULL
 );
 
-
 -- CREATE JUNCTION TABLE
 -- You will need to create the junction table that stores the relationships between "movies" and "genres"
 -- This table will need to be populated with some data as well (INSERTS)
 -- Recall that this Junction Table will just be a table of ids!
-
+CREATE TABLE "movies_genres" (
+	"id" SERIAL PRIMARY KEY,
+	"movies_id" INT REFERENCES "movies",
+	"genres_id" INT REFERENCES "genres"
+);
 
 
 --------[ DATA! ]---------
@@ -55,3 +58,12 @@ VALUES
 ('Science Fiction'),
 ('Space-Opera'),
 ('Superhero');
+
+-- Starter Movie Genres
+INSERT INTO "movies_genres" ("movies_id", "genres_id")
+VALUES 
+(1, 2),
+(1, 11),
+(2, 2),
+(2, 10),
+(3, 13);
