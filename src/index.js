9cc372e4a8165af6b27064936810220cee1c07ac
Myron <dubtwee@gmail.com>
2020-04-28 09:59:52 -0500

    removing code that is no longer needed

--- src/index.js
+++ src/index.js
@@ -9,80 +9,9 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 // Import saga middleware
 import createSagaMiddleware from 'redux-saga';
-import axios from 'axios';
-import { put, takeLatest } from 'redux-saga/effects';
 
 import rootReducer from './redux/reducers/_root.reducer';
 import rootSaga from './redux/sagas/_root.saga';
-
-//
-// SAGAS for API calls
-// --------------------
-
-// SAGA GETs all movies from server API and sends to movies reducer
-// function* getAllMovies(action) {
-//     try {
-//         const response = yield axios.get('/api/movies');
-//         yield put({
-//             type: 'SET_MOVIES',
-//             payload: response.data,
-//         }); // put() is the same as this.props.dispatch()
-//     } catch(err) {
-//         console.warn(err);
-//     }
-// }
-
-// function* getMovie(action) {
-//     try {
-//         const movieId = action.payload;
-//         const response = yield axios.get(`/api/movies/details/${movieId}`);
-//         yield put({
-//             type: 'SET_DETAILS',
-//             payload: response.data[0],
-//         }); // put() is the same as this.props.dispatch()
-//     } catch(err) {
-//         console.warn(err);
-//     }
-// }
-
-// function* getMovieGenres(action) {
-//     try {
-//         const movieId = action.payload;
-//         const response = yield axios.get(`/api/movies/genres/${movieId}`);
-//         yield put({
-//             type: 'SET_GENRES',
-//             payload: response.data,
-//         })
-//     } catch(err) {
-//         console.warn(err);
-//     }
-// }
-
-// function* putMovieDetails(action) {
-//     try {
-//         const movieId = action.payload.id;
-//         yield axios.put(`/api/movies/edit/${movieId}`, action.payload);
-//         yield put({
-//             type: 'GET_MOVIE',
-//             payload: movieId,
-//         });
-//         yield put({
-//             type: 'GET_MOVIE_GENRES',
-//             payload: movieId,
-//         });
-//     } catch(err) {
-//         console.warn(err);
-//     }
-// }
-
-// Create the rootSaga generator function
-// function* rootSaga() {
-//     // REGISTER SAGAS HERE
-//     yield takeLatest('GET_ALL_MOVIES', getAllMovies);
-//     yield takeLatest('GET_MOVIE', getMovie);
-//     yield takeLatest('GET_MOVIE_GENRES', getMovieGenres);
-//     yield takeLatest('PUT_MOVIE', putMovieDetails);
-// }
 
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
