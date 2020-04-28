b7c3ca61382520318efdd1e992f22e85ad3aa85f
Myron <dubtwee@gmail.com>
2020-04-28 12:13:26 -0500

    allowing user to search movies list and show results on Home view

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -4,6 +4,10 @@ import { withRouter } from 'react-router-dom';
 import MoviesListItem from '../MoviesListItem/MoviesListItem';
 
 class MoviesList extends Component {
+  state = {
+    searchTerm: '',
+  }
+
   componentDidMount() {
     // load up all information from the server
     this.props.dispatch({
@@ -15,13 +19,34 @@ class MoviesList extends Component {
     this.props.history.push(`/details/${id}`);
   }
 
+  changeSearch = (event) => {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  }
+
   render() {
-    const limitedResults = this.props.store.movies.filter((item, index) => {
+    let limitedResults = this.props.store.movies.filter((item, index) => {
+      const lowerTitle = item.title.toLowerCase();
+
+      if (this.state.searchTerm) {
+        return lowerTitle.indexOf(this.state.searchTerm.toLowerCase()) !== -1;
+      }
+
+      return true;
+    });
+
+    limitedResults = limitedResults.filter((item, index) => {
       return index < 10;
     });
 
     return (
       <div className="algnLeft">
+        <input
+          placeholder="Search"
+          type="text"
+          onChange={this.changeSearch}
+        />
         {limitedResults.map((item, index) => (
           <MoviesListItem key={index} item={item} />
         ))}
