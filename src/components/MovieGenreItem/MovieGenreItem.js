15e65c1084eae0a08b88dbf31a2df4385ab5fe96
Myron <dubtwee@gmail.com>
2020-04-28 10:58:33 -0500

    capturing click of a newly added delete button to the genres on Edit view

--- src/components/MovieGenreItem/MovieGenreItem.js
+++ src/components/MovieGenreItem/MovieGenreItem.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class MovieGenreItem extends Component {
+  clickDeleteGenre = (event) => {
+    // dispatch to a saga for deleting genre from database
+    console.log('CLICKED DELETE');
+  }
+
   render() {
     const { item } = this.props;
     return (
       <li>
-        {item.name}
+        {item.name} <button onClick={this.clickDeleteGenre}>Delete</button>
       </li>
     );
   }
