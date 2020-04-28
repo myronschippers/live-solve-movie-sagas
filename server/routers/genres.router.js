d03e758bacbd8bbe0ce021b3c30ad2e37b7d961d
Myron <dubtwee@gmail.com>
2020-04-28 13:11:45 -0500

    stubbing in post and delete endpoints for genres

--- server/routers/genres.router.js
+++ server/routers/genres.router.js
@@ -17,5 +17,9 @@ router.get('/', (req, res) => {
 });
 
 // TODO - Create delete endpoint
+router.delete('/:id', (req, res) => {});
+
+// add new genres
+router.post('/', (req, res) => {});
 
 module.exports = router;
