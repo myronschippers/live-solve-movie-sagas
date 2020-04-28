147e45ad39e5a4f28467b59af954124a3f7c2949
Myron <dubtwee@gmail.com>
2020-04-27 23:00:58 -0500

    creating placeholder page route on FE for Details view

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+// PAGE COMPONENTS
+import DetailsPage from '../pages/DetailsPage/DetailsPage';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <Router>
+          {/* <Route exact path="/" component={DetailsPage} /> */}
+          <Route path="/details" component={DetailsPage} />
+          {/* <Route path="/edit" component={DetailsPage} /> */}
+        </Router>
       </div>
     );
   }
---
+++ src/components/pages/DetailsPage/DetailsPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DetailsPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Details</h2>
+      </div>
+    );
+  }
+}
+
+export default DetailsPage;
