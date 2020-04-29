df68f464982f85c60c9454dad239f56d2e9ab7bb
Myron <dubtwee@gmail.com>
2020-04-28 21:26:41 -0500

    creating API endpoints for the login authentication

---
+++ server/routers/auth.router.js
@@ -0,0 +1,44 @@
+// this is actually not secure at all
+const express = require('express');
+const pool = require('../modules/pool');
+
+const router = express.Router();
+
+const credentials = {
+  id: 1,
+  username: 'camera',
+  password: 'action',
+  isLoggedIn: false,
+};
+
+router.get('/user', (req, res) => {
+  if (credentials.isLoggedIn) {
+    res.send({
+      username: credentials.username,
+      id: credentials.id,
+    })
+  } else {
+    res.sendStatus(500);
+  }
+});
+
+router.post('/login', (req, res) => {
+  const loginCreds = req.body;
+
+  if (loginCreds.username === credentials.username
+    && loginCreds.password === credentials.password
+  ) {
+    credentials.isLoggedIn = true;
+    res.send(200);
+  } else {
+    credentials.isLoggedIn = false;
+    res.send(500);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  credentials.isLoggedIn = false;
+  res.send(200);
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 const moviesRouter = require('./routers/movies.router');
 const genresRouter = require('./routers/genres.router');
+const authRouter = require('./routers/auth.router');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
@@ -14,6 +15,7 @@ app.use(express.static('build'));
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
 // create / register new login route
+app.use('/api/auth', authRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
