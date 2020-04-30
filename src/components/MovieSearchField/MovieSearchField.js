5eaa031b2be094e4d829d57771310bcf0cbfd765
Myron <dubtwee@gmail.com>
2020-04-29 22:32:30 -0500

    adding custom serch field styling from material-ui

--- src/components/MovieSearchField/MovieSearchField.js
+++ src/components/MovieSearchField/MovieSearchField.js
@@ -1,6 +1,54 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { withStyles, createStyles, fade } from '@material-ui/core/styles';
+// material-ui component imports
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+
+const customStyles = theme =>
+  createStyles({
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.2),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  });
+
 class MovieSearchField extends Component {
   changeSearch = (event) => {
     this.props.dispatch({
@@ -10,14 +58,27 @@ class MovieSearchField extends Component {
   }
 
   render() {
+    const {
+      classes
+    } = this.props;
+
     return (
-      <input
-        placeholder="Search"
-        type="text"
-        onChange={this.changeSearch}
-      />
+      <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+        <InputBase
+          placeholder="Search…"
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput,
+          }}
+          inputProps={{ 'aria-label': 'search' }}
+          onChange={this.changeSearch}
+        />
+      </div>
     );
   }
 }
 
-export default connect()(MovieSearchField);
+export default withStyles(customStyles)(connect()(MovieSearchField));
