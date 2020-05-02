713a13f05563260a0bbaccca5db3791e942aca90
Myron <dubtwee@gmail.com>
2020-05-02 14:42:10 -0500

    adding Card component for styling the Movie items listed on Movie List view

--- src/components/MoviesListItem/MoviesListItem.js
+++ src/components/MoviesListItem/MoviesListItem.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
+// dependencies for custom material-ui styling
+import { withStyles, createStyles } from '@material-ui/core/styles';
+// material-ui components
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia
+} from '@material-ui/core';
+
+// create custom material styling
+const customStyles = theme =>
+  createStyles({
+    imgMedia: {
+      height: '335px',
+      backgroundSizing: 'cover',
+    }
+  });
+
 class MoviesListItem extends Component {
   componentDidMount() {
     // load up all information from the server
@@ -15,29 +34,41 @@ class MoviesListItem extends Component {
   }
 
   render() {
-    const { item } = this.props;
+    const {
+      item,
+      classes,
+    } = this.props;
 
     return (
-      <div
-        className="movieListItem"
-        onClick={(event) => this.clickMovieDetails(event, item.id)}
-      >
-        <img src={item.poster} alt={item.title} />
-        <div>
-          <h3>{item.title}</h3>
-          <p>{item.description}</p>
-          <ul>
-            {item.genre.filter((genreItemFilter) => (genreItemFilter !== null))
-              .map((genreItem, genreIndex) => (
-              <li key={genreIndex}>{genreItem}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      <Card>
+        <CardActionArea
+          onClick={(event) => this.clickMovieDetails(event, item.id)}
+        >
+          <CardMedia
+            className={classes.imgMedia}
+            image={item.poster}
+            title={item.title}
+          />
+          <CardContent>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <ul>
+              {item.genre.filter((genreItemFilter) => (genreItemFilter !== null))
+                .map((genreItem, genreIndex) => (
+                <li key={genreIndex}>{genreItem}</li>
+              ))}
+            </ul>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     );
   }
 }
 
 const mapStoreToProps = store => ({ store });
 
-export default withRouter(connect(mapStoreToProps)(MoviesListItem));
+export default withStyles(customStyles)(
+  withRouter(
+    connect(mapStoreToProps)(MoviesListItem)
+  )
+);
