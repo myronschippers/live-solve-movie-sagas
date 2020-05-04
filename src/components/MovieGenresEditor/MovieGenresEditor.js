5f2a1f2ba2102bc257791efccbd11feec9ae9133
Myron <dubtwee@gmail.com>
2020-05-03 21:59:40 -0500

    changing the layout of the genres block on the Edit view

--- src/components/MovieGenresEditor/MovieGenresEditor.js
+++ src/components/MovieGenresEditor/MovieGenresEditor.js
@@ -8,28 +8,52 @@ import { withStyles, createStyles } from '@material-ui/core/styles';
 import {
   Paper,
   IconButton,
+  Modal,
+  Button,
+  Typography,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Box,
 } from '@material-ui/core';
 import { AddCircle } from '@material-ui/icons';
 
 const customStyles = theme =>
   createStyles({
     root: {
+      padding: theme.spacing(2),
+    },
+    pillList: {
       display: 'flex',
       justifyContent: 'flex-start',
       alignItems: 'center',
       flexWrap: 'wrap',
       '& > *': {
-        margin: theme.spacing(0.5),
+        margin: theme.spacing(0, 0.5, 0.5, 0),
       },
       margin: 0,
-      padding: theme.spacing(1),
+      padding: theme.spacing(0, 0, 1, 0),
       listStyle: 'none',
+    },
+    modalContent: {
+      width: '350px',
+      height: '200px',
+      top: `50%`,
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      position: 'fixed',
+      padding: theme.spacing(2, 3, 3),
+    },
+    formControl: {
+      minWidth: 120,
     },
   });
 
 class MovieGenresEditor extends Component {
   state = {
     selectedGenreId: 0,
+    isAddGenreModalOpen: false,
   }
 
   componentDidMount() {
@@ -60,6 +84,22 @@ class MovieGenresEditor extends Component {
         movies_id: this.props.movieId,
         genres_id: this.state.selectedGenreId,
       }
+    });
+
+    this.closeAddGenreModal();
+  }
+
+  // updating local state to close modal
+  closeAddGenreModal = () => {
+    this.setState({
+      isAddGenreModalOpen: false,
+    })
+  }
+
+  // updating local state to open modal
+  openAddGenreModal = () => {
+    this.setState({
+      isAddGenreModalOpen: true,
     })
   }
 
@@ -70,8 +110,8 @@ class MovieGenresEditor extends Component {
 
     return (
       <div>
-        <h4>Movie Genre(s)</h4>
-        <div>
+        {/* <h4>Movie Genre(s)</h4> */}
+        {/* <div>
           <select onChange={this.changeSelectedGenre}>
             <option value="">Select a Genre</option>
             {this.props.store.genres.map((item, index) => (
@@ -79,22 +119,84 @@ class MovieGenresEditor extends Component {
             ))}
           </select>
           <button onClick={this.clickAddGenre}>Add Genre</button>
-        </div>
+        </div> */}
 
         <Paper
-          component="ul"
+          component="div"
           variant="outlined"
           className={classes.root}
         >
-          {this.props.store.movieGenres.map((item, index) => (
-            <MovieGenreItem key={index} item={item} />
-          ))}
-          <li>
-            <IconButton onClick={this.clickAddGenre}>
-              <AddCircle />
-            </IconButton>
-          </li>
+          <Typography
+            variant="subtitle1"
+            component="h4"
+          >
+            Genres:
+          </Typography>
+
+          <ul className={classes.pillList}>
+            {this.props.store.movieGenres.map((item, index) => (
+              <MovieGenreItem key={index} item={item} />
+            ))}
+            <li>
+              <IconButton onClick={this.openAddGenreModal}>
+                <AddCircle />
+              </IconButton>
+            </li>
+          </ul>
         </Paper>
+
+        <Modal
+          open={this.state.isAddGenreModalOpen}
+          onClose={this.closeAddGenreModal}
+        >
+          <Paper className={classes.modalContent}>
+            <Typography
+              component="h3"
+              variant="h6"
+              gutterBottom
+            >
+              Pick a New Genre to Add
+            </Typography>
+
+            <Box mb={3}>
+              <FormControl
+                variant="outlined"
+                className={classes.formControl}
+                fullWidth
+              >
+                <InputLabel id="genresSelection">Genres:</InputLabel>
+                <Select
+                  labelId="genresSelection"
+                  id="genresSelection"
+                  onChange={this.changeSelectedGenre}
+                  label="Genres:"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  {this.props.store.genres.map((item, index) => (
+                    <MenuItem key={index} value={item.id}>{item.name}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+
+            {/* <select onChange={this.changeSelectedGenre}>
+              <option value="">Select a Genre</option>
+              {this.props.store.genres.map((item, index) => (
+                <option key={index} value={item.id}>{item.name}</option>
+              ))}
+            </select> */}
+            <Button
+              onClick={this.clickAddGenre}
+              variant="contained"
+              color="primary"
+              fullWidth
+            >
+              Save
+            </Button>
+          </Paper>
+        </Modal>
       </div>
     );
   }
